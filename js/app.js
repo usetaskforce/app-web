91c4b8dbdec94a21144d1898bcd272ed42a0cd94
Marcus Durst <marcusdurst@mail.com>
2016-05-03 18:37:29 -0500

    version 0.0.1

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.bxslider').bxSlider({
+   	auto: true,
+  	mode: 'fade',
+    pager: false,
+  	controls: false,
+  	autoControls: false,
+  	autoHover: true
+  });
+});
